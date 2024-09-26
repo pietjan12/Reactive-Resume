a6ed1d3379165e59d6ab0c245504295ee21438ee
Nick van der Burgt <nvanderburgt@infiniot.nl>
2024-09-26 15:25:17 +0200

    clean repo

--- apps/client/src/services/auth/refresh.ts
+++
@@ -1,8 +0,0 @@
-import { MessageDto } from "@reactive-resume/dto";
-import { AxiosInstance, AxiosResponse } from "axios";
-
-export const refreshToken = async (axios: AxiosInstance) => {
-  const response = await axios.post<MessageDto, AxiosResponse<MessageDto>>("/auth/refresh");
-
-  return response.data;
-};
--- libs/dto/src/feature/index.ts
+++
@@ -1,9 +0,0 @@
-import { createZodDto } from "nestjs-zod/dto";
-import { z } from "nestjs-zod/z";
-
-export const featureSchema = z.object({
-  isSignupsDisabled: z.boolean().default(false),
-  isEmailAuthDisabled: z.boolean().default(false),
-});
-
-export class FeatureDto extends createZodDto(featureSchema) {}
